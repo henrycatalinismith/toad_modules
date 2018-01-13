042f92f847a9241f77548e6870fe2159ea4f203c
Henry Catalini Smith <henry@catalinismith.com>
2018-01-13 19:47:07 +0100

    add total and reverse order

--- bin/toad_modules.js
+++ bin/toad_modules.js
@@ -4,12 +4,18 @@ const getSize = require("get-folder-size");
 const { sprintf } = require("sprintf");
 
 let [ target ] = process.argv.slice(2);
+if (!target) {
+  target = process.cwd();
+}
 if (target[target.length - 1] === "/") {
   target = target.substring(0, target.length - 1);
 }
 
 const regex = 'node_modules$';
 const command = `find ${target} -type d | grep "${regex}"`;
+
+let total = 0;
+let longest;
 
 exec(command, (error, stdout, stderr) => {
   const roots = stdout.split("\n").filter(path => {
@@ -19,14 +25,19 @@ exec(command, (error, stdout, stderr) => {
 
   const labels = roots.map(path => path.substring(target.length + 1));
 
-  const longest = labels.reduce((prev,cur) => {
+  longest = labels.reduce((prev,cur) => {
     return prev.length > cur.length ? prev : cur;
   }, "");
 
   roots.forEach((path, i) => {
     getSize(path, (error, size) => {
-      console.log(sprintf(`%-${longest.length}s %dM`, labels[i], size / 1024 / 1024));
+      total += size;
+      console.log(sprintf(`%dM %-${longest.length}s`, size / 1024 / 1024, labels[i]));
     });
   });
+});
+
+process.on('exit', () => {
+  console.log(sprintf(`%dM %-${longest.length}s`, total / 1024 / 1024, "TOTAL"));
 });
 
